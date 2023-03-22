823ce8e09028c10f4beeb5496c848763a7710daa
Giovani <giakomogcs@gmail.com>
2023-03-21 22:32:03 -0300

    middleware no user

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -2,10 +2,12 @@ const {Router} = require("express");
 const UserController = require("../controllers/UserController");
 const usersRoutes = Router();
 
+const ensureAuthenticated = require("../middleware/ensureAuthenticated")
+
 const userController = new UserController();
 
 usersRoutes.post("/", userController.create);
-usersRoutes.put("/:id", userController.update)
+usersRoutes.put("/", ensureAuthenticated, userController.update)
 
 
 module.exports = usersRoutes;
